a7c7a46e9e0f755cad251cf8cd7be8c0c2810b92
Kareem <k2gees@gmail.com>
2020-08-15 16:24:38 -0500

    js adjusted to accept incoming changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
 // generate random password
 
-<<<<<<< HEAD
-// function generate(){
-
-  //set password length/
-=======
 console.log("Loading")
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
          
   var specialCharacters = [
     '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',  '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'
@@ -24,18 +18,8 @@ console.log("Loading")
   ]
   
   function getPasswordOption(){
-<<<<<<< HEAD
-    var length = parseInt (
-      prompt("How many characters would you like your password to be?")
-    )
-      if (
-        isNaN(length) === true
-      )
-      {
-=======
     var length = parseInt (prompt("How many characters would you like your password to be?"))
       if (isNaN(length) === true){
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
         alert("Has to be a number between 8-128!")
         return
       }
@@ -43,12 +27,7 @@ console.log("Loading")
         alert("The password needs to be at least 8 special characters.")
         return
       }
-<<<<<<< HEAD
-      if (
-        length > 128){
-=======
       if (length > 128){
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
         alert("The password has to be less than 128 characters.")
         return
       }
@@ -56,30 +35,14 @@ console.log("Loading")
       var lowers = confirm("Would you like to use lowercase letters?");
       var numbers = confirm("Would you like to use numbers?");
       var symbols = confirm("Would you like to use special characters?");
-<<<<<<< HEAD
-  
-  
-      if(
-        uppers === false && 
-        lowers === false &&
-        numbers === false && 
-        symbols === false 
-  
-      ){
-=======
       var everythingNotSelected = uppers === false && lowers === false && numbers === false && symbols === false
   
       if(everythingNotSelected){
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
         alert("You must select at least one.")
         return
       }
       var userInput = {
-<<<<<<< HEAD
-        length: length, uppers: uppers, lowers: lowers, numbers: numbers, symbols: symbols
-=======
      length: length, uppers: uppers, lowers: lowers, numbers: numbers, symbols: symbols
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
       }
       return userInput
   
@@ -98,7 +61,6 @@ console.log("Loading")
     var options = getPasswordOption()
      
     var result = []
-<<<<<<< HEAD
   
     var possibleCharacters = []
   
@@ -131,57 +93,6 @@ console.log("Loading")
   
   
   }
-  
-  
-  
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
-  
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-  }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-=======
-  
-    var possibleCharacters = []
-  
-    var guaranteeCharacters = []
-  
-    if(options.uppers){
-      possibleCharacters = possibleCharacters.concat(upperCase)
-      guaranteeCharacters.push(getRandomValue (upperCase))
-    }
-    if(options.lowers){
-      possibleCharacters = possibleCharacters.concat(lowerCase)
-      guaranteeCharacters.push(getRandomValue (lowerCase))
-    }
-    if(options.numbers){
-      possibleCharacters = possibleCharacters.concat(numbers)
-      guaranteeCharacters.push(getRandomValue (numbers))
-    } 
-    if(options.symbols){
-      possibleCharacters = possibleCharacters.concat(specialCharacters)
-      guaranteeCharacters.push(getRandomValue (specialCharacters))
-    }
-    for(var i = 0; i < options.length; i++){
-      var possibleCharacter = getRandomValue(possibleCharacters)
-      result.push(possibleCharacter)
-    }
-    for(var i = 0; i < guaranteeCharacters.length; i++){
-      result[i] = guaranteeCharacters[i]
-    }
-    return result.join("")
-  
-  
-  }
->>>>>>> 85a66a92bb8efe07e9c53b5d0c3f9b3930aa7641
   
   
   
